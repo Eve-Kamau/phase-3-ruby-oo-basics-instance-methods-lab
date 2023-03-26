0ec23517624b0f882af949ee47310e2b078c9b6f
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2023-03-26 20:03:58 +0300

    Completed Lab

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+    def talk
+      puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+end
+  
+eva = Person.new
+eva.talk 
+eva.walk 
+
+barry = Person.new
+barry.talk 
+barry.walk 
